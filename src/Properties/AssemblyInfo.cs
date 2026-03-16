696e1e449952975aecfffbdf82d2a141a98164c0
validaq <validaq@gmail.com>
2026-03-16 09:51:36 +0100

    Add AssemblyInfo

---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("QM_CloneSort")]
+[assembly: AssemblyDescription("Clone Sort mod for Quasimorph")]
+[assembly: AssemblyCompany("ValidAQ")]
+[assembly: AssemblyProduct("QM_CloneSort")]
+[assembly: AssemblyCopyright("Copyright © ValidAQ")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
